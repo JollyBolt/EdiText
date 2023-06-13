977a6ba013232304fc89e69c98a663caa459e523
Ishan <Braviaspan2002@gmail.com>
2023-06-13 18:15:00 +0530

    Setting Up COntact Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="container fixed-bottom">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <div className="col-md-4 d-flex align-items-center">
-                    <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                    <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1 fw-bold">
                         EdiText
                     </a>
                     <span className="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
