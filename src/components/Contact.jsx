fc75860223952b8a179620e7568a597e108df5e7
Ishan <Braviaspan2002@gmail.com>
2023-06-21 23:05:44 +0530

    Revamped Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,60 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function Contact() {
+    const [formState, setFormState] = useState({})
+
+    const handleChange = (e) =>{
+        setFormState({...formState,[e.target.name]:e.target.value})
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        // console.log(formState)
+        Email.send({
+            SecureToken : "3b782a7d-b44c-4b8e-89a7-a472f5a0692b",
+            To : 'fguy256@gmail.com',
+            From : 'fguy256@gmail.com',
+            Subject : "This is the subject",
+            Body : formState.message
+        }).then(
+          message => alert(message)
+        );
+        Email.send({
+            SecureToken : "3b782a7d-b44c-4b8e-89a7-a472f5a0692b",
+            To : formState.email,
+            From : 'fguy256@gmail.com',
+            Subject : "Regarding Your Query",
+            Body : "We have recieved your query and you will be contacted soon."
+        }).then(
+          
+        );
+
+    }
     return (
         <div className='container mt-5'>
-            <form className="row g-3">
-                <div className="col-md-6">
-                    <label htmlFor="inputEmail4" className="form-label">First Name</label>
-                    <input type="text" className="form-control" id="fistName" />
+            <div className='row'>
+                <div className='col-lg-6 col-sm-12 p-3 d-flex align-items-center'>
+                    <h1 className="display-1 fw-bolder ">Send<br/>Us a<br/>Message.</h1>
                 </div>
-                <div className="col-md-6">
-                    <label htmlFor="lastName" className="form-label">Last Name</label>
-                    <input type="text" className="form-control" id="lastName" />
+                <div className='col-lg-6 col-sm-12 p-3'>
+                    <form className="" onSubmit={submitHandler}>
+                        <div className="">
+                            <label htmlFor="inputEmail4" className="form-label">First Name</label>
+                            <input type="text" name='firstName' className="form-control" id="fistName" value={formState.firstName} onChange={handleChange} />
+                        </div>
+                        <div className="">
+                            <label htmlFor="lastName" className="form-label">Last Name</label>
+                            <input type="text" name='lastName' className="form-control" id="lastName" value={formState.lastName} onChange={handleChange} />
+                        </div>
+                        <div className="">
+                            <label htmlFor="inputEmail" className="form-label">Email</label>
+                            <input type="email" name='email' className="form-control" id="inputEmail" value={formState.email} onChange={handleChange} />
+                        </div>
+                        <div className="">
+                            <label htmlFor="inputMessage" className="form-label">Message</label>
+                            <textarea className="form-control" name='message' id="inputMessage" rows="8" value={formState.message} onChange={handleChange}></textarea>
+                        </div>
+                        <div className=" text-center mt-2">
+                            <button type="submit" className="btn btn-primary px-5">Send</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="col-md-6">
-                    <label htmlFor="inputEmail4" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4" />
-                </div>
-                <div className="col-md-6">
-                    <label htmlFor="inputPassword4" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="inputPassword4" />
-                </div>
-                <div className="col-12">
-                    <label htmlFor="inputAddress" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-                </div>
-                <div className="col-12">
-                    <label htmlFor="inputAddress2" className="form-label">Address 2</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                </div>
-                <div className="col-md-6">
-                    <label htmlFor="inputCity" className="form-label">City</label>
-                    <input type="text" className="form-control" id="inputCity" />
-                </div>
-                <div className="col-md-4">
-                    <label htmlFor="inputState" className="form-label">State</label>
-                    <select id="inputState" className="form-select">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                    </select>
-                </div>
-                <div className="col-md-2">
-                    <label htmlFor="inputZip" className="form-label">Zip</label>
-                    <input type="text" className="form-control" id="inputZip" />
-                </div>
-                {/* <div className="col-12">
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" id="gridCheck" />
-                        <label className="form-check-label" htmlFor="gridCheck">
-                            Check me out
-                        </label>
-                    </div>
-                </div> */}
-                <div className="col-12 text-center">
-                    <button type="submit" className="btn btn-primary px-5">Send</button>
-                </div>
-            </form>
+            </div>
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className="container fixed-bottom">
+        <div className="container ">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <div className="col-md-4 d-flex align-items-center">
                     <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1 fw-bold">
